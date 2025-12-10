af52318b7340323923eee4e25586e3286be0d30b
Asjad Saqib <vforce.asjad@gmail.com>
2025-12-10 17:55:35 +0200

    Added Some level elements and custom Music

--- GodotFinalAssignment/brackeys_platformer_assets/Scripts/game_manager.gd
+++ GodotFinalAssignment/brackeys_platformer_assets/Scripts/game_manager.gd
@@ -3,6 +3,7 @@ class_name GameManager
 var score: int = 0
 
 @onready var coins: Label = $Collected_Coins
+@onready var collected_coins_2: Label = %Collected_Coins2
 
 func add_point() -> void:
 	score += 1
