fe42feff237db2f62a1d93ab791e6c33cea60beb
Asjad Saqib <vforce.asjad@gmail.com>
2025-12-10 15:57:51 +0200

    Added Level Switching functionality.

--- GodotFinalAssignment/teleport.gd
+++ GodotFinalAssignment/teleport.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var can_tp: = false
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player": # Good practice: Make sure it's actually the player
 		print("Press F to interact")
@@ -15,4 +16,9 @@ func _on_body_exited(body: Node2D) -> void:
 func _process(delta: float) -> void:
 	if can_tp and Input.is_action_just_pressed("Interact"):
 		print("You interacted!")
+		var current_scene_file: = get_tree().current_scene.scene_file_path
+		var next_level_number: = current_scene_file.to_int() + 1
+		var next_level_path: = "res://Level_" + str(next_level_number) + ".tscn"
+		get_tree().change_scene_to_file(next_level_path)
+		
 		
